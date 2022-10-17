e1dd729355eb0fdfb90e688c910a85898f86b69a
Bruno Cocco <brunoe.cocco@gmail.com>
2022-10-16 22:04:45 -0300

    agregando logica

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const nuevaTirada = document.getElementById("tirar-dado");
 nuevaTirada.addEventListener("click", () => {
     console.log("click")
-    
+    let numeroDado = parseInt(Math.random() * 6 + 1)
+    console.log(numeroDado)
+    let printDado = document.getElementById("printear--dado")
+    printDado.append(`
+    ${numeroDado}
+    `)
 })
