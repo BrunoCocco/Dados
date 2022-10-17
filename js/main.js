a154d71f2c9a1a095c013316b647c65c2492d468
Bruno Cocco <brunoe.cocco@gmail.com>
2022-10-17 16:05:18 -0300

    agregando nuevas funcinavilidades

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
+// const nuevaTirada = document.getElementById("tirar-dado");
+// nuevaTirada.addEventListener("click", () => {
+//     console.log("click")
+//     let numeroDado = parseInt(Math.random() * 6 + 1)
+//     console.log(numeroDado)
+//     let printDado = document.getElementById("printear--dado")
+//     printDado.appendChild(`
+//     ${numeroDado}
+//     `)
+// })
+
 const nuevaTirada = document.getElementById("tirar-dado");
 nuevaTirada.addEventListener("click", () => {
     console.log("click")
     let numeroDado = parseInt(Math.random() * 6 + 1)
     console.log(numeroDado)
-    let printDado = document.getElementById("printear--dado")
-    printDado.append(`
-    ${numeroDado}
-    `)
+    const agregarNumeroAlDom = document.createElement(`h1`)
+    agregarNumeroAlDom.appendChild
 })
+
+
 
 const borrarTirada = document.getElementById("borrar-dado");
 borrarTirada.addEventListener("click", () => {
